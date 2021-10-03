a6e73a2795a0587e227c8d845a649b85723b754d
thomsoon <amagasa.tomoya@gmail.com>
2021-10-03 19:15:04 +0900

    refactor: Introduced regular expressions and changed the detection rules.

--- content.js
+++ content.js
@@ -1,63 +1,80 @@
 $(function(){
-    //type="submit"のinputタグがクリックされたときにイベントが発火
-    $('input[type="submit"]').on('click', function(event) {
-        //入力されたクレデンシャル情報を格納
-        let credentials = [];
+    //inputタグに文字が入力されるたびにイベントが発火
+    $("body").on('input', "input", function(event){
+        //入力値のハッシュと，保存されているクレデンシャルのハッシュを比較
 
-        //type="password"のinputタグに入力されたクレデンシャル情報を収集
-        $('input[type="password"]:enabled').each(function(index,element){
-            if(element.value){
-                credentials.push("password:" +element.value);
-            }
-        });
-
-        //type="email"のinputタグに入力されたクレデンシャル情報を収集
-        $('input[type="email"]:enabled').each(function(index,element){
-            if(element.value){
-                credentials.push("email:" +element.value);
-            }
-        });
-
-        //type="tel"のinputタグに入力されたクレデンシャル情報を収集
-        $('input[type="tel"]:enabled').each(function(index,element){
-            if(element.value){
-                credentials.push("phone number:" +element.value);
-            }
-        });
-
-        //type="text","number"のinputタグに入力されたクレデンシャル情報を収集
-        $('input[type="text"],[type="number"]:enabled').each(function(index,element){
-            if(element.value){
-                //name属性からクレデンシャル情報の属性を抽出
-                //ToDo:正規表現に変える
-                if(element.name == "name" || element.name == "fname" || element.name == "mname" || element.name == "lname"){
-                    credentials.push("name:"+element.value);
-                }
-                else if(element.name == "email"){
-                    credentials.push("email:" + element.value);
-                }
-                else if(element.name == "address" || element.name == "city"){
-                    credentials.push("address:" + element.value);
-                }
-                else if(element.name == "phone" || element.name == "mobile" || element.name == "country-code" || element.name == "area-code" ){
-                    credentials.push("phone number:" + element.value);
-                }
-                else if(element.name == "ccname" || element.name == "cardnumber" || element.name == "cvc" || element.name == "ccmonth" || element.name == "ccyear" || element.name == "exp-date" || element.name == "card-type"){
-                    credentials.push("credit card:" + element.value);
-                }
-                else if(element.name == "username"){
-                    credentials.push("username:" + element.value);
-                }
-
-            }
-        });
-
-        //クレデンシャル情報が入力されているのを確認出来たら，アラートを表示する
-        if(credentials.length){
-            alert("The following credential information was entered.\n"+credentials);
+        //一致したらアラートを出す
+        if(false){
+           alert("保存されたクレデンシャル情報と入力値が一致しました．"); 
         }
-        
-    });    
     
+    });
+
+    //Todo:動的に生成されたIframe中のイベントを取得する
+    /*
+    $("iframe").on('load', function(){
+        const jsInitCheckTimer = setInterval(jsLoaded, 1000);
+        function jsLoaded() {
+            if ($("body").contents().find("iframe").find("input") == null) {
+                clearInterval(jsInitCheckTimer);
+            }
+            console.log($("body").contents().find("iframe").find("input"));
+        }
+//        console.log($("body").contents().find("div").find("input"));
+    });*/
+
     
+    var flag = false;
+    //inputタグに文字が入力され，フォーカスが外れたタイミングでイベントが発火
+    $("body").on('change', "input", function(event) {
+        //アラートは一つのページで一度しか表示しない
+        if(!flag){
+             //入力されたクレデンシャル情報を格納
+            let credentials = [];
+
+            //type="password"のinputタグに入力されたクレデンシャル情報を収集
+            $('input[type="password"]:enabled').each(function(index,element){
+                if(element.value){
+                    credentials.push("password:" +element.value);
+                }
+            });
+
+            //type="email"のinputタグに入力されたクレデンシャル情報を収集
+            $('input[type="email"]:enabled').each(function(index,element){
+                if(element.value){
+                    credentials.push("email:" +element.value);
+                }
+            });
+
+            //type="text","number","tel"のinputタグに入力されたクレデンシャル情報を収集
+            $('input[type="text"],[type="number"],[type="tel"]:enabled').each(function(index,element){
+                let name = element.name;
+                if(element.value){
+                    //name属性からクレデンシャル情報の属性を抽出                    
+                    if(name.match(/^[\w/:%#\$&\?\(\)~\.=\+\-]*name[\w/:%#\$&\?\(\)~\.=\+\-]*$/)!=null){
+                        credentials.push("name:"+element.value);
+                    }
+                    else if(name.match(/^[\w/:%#\$&\?\(\)~\.=\+\-]*email[\w/:%#\$&\?\(\)~\.=\+\-]*$/)!=null){
+                        credentials.push("email:" + element.value);
+                    }
+                    else if(name.match(/^[\w/:%#\$&\?\(\)~\.=\+\-]*address[\w/:%#\$&\?\(\)~\.=\+\-]*$/)!=null){
+                        credentials.push("address:" + element.value);
+                    }
+                    else if(name.match(/^[\w/:%#\$&\?\(\)~\.=\+\-]*phone[\w/:%#\$&\?\(\)~\.=\+\-]*$/)!=null){
+                        credentials.push("phone number:" + element.value);
+                    }
+                    else if(name.match(/^[\w/:%#\$&\?\(\)~\.=\+\-]*credit( ?card)?[\w/:%#\$&\?\(\)~\.=\+\-]*$/)!=null){
+                        credentials.push("credit card:" + element.value);
+                    }
+                }
+            });
+
+            //クレデンシャル情報が入力されているのを確認出来たら，アラートを表示する
+            if(credentials.length){
+                alert("以下のクレデンシャル情報を入力しようとしています．\n"+credentials);
+                
+                flag = true;
+            }
+        }
+    });
 });
